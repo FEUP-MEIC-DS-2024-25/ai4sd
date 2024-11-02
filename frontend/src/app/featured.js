fff7edda350e981ea917b3681f963cb9ed4366d1
lessthelonely <42583814+lessthelonely@users.noreply.github.com>
2024-11-02 16:25:57 +0000

    Update featured.js

--- frontend/src/app/featured.js
+++ frontend/src/app/featured.js
@@ -36,6 +36,7 @@ export default function Featured(props) {
             <div className="header" style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "start", padding: "20px 20px 10px 20px"}}>
                 <h4 className="card-title" style={{padding: "0px", margin: 0 + "px", marginRight: 5 + "px"}}>{props.name}</h4>
                 <p className="card-text" style={{padding: 0 + "px", color: "#C4C4C4"}}>by {props.teamId}</p>
+
             </div>
             <div className="featured-body" style={{padding: "20px", paddingTop: "0px", paddingBottom: "0px", margin: "0px 0px 10px 0px"}}>
                 <p className="card-text" style={{textAlign: "justify", color: "#636363"}}>{props.description}</p>
