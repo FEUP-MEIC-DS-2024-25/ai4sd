6c74d0aa888b95ad5a7bdc7d4b8bf98fb18f4bde
lessthelonely <42583814+lessthelonely@users.noreply.github.com>
2024-11-02 16:12:38 +0000

    Add footer to featured card.

--- frontend/src/app/featured.js
+++ frontend/src/app/featured.js
@@ -1,17 +1,48 @@
 import Image from "next/image";
 
+function getAccentColor(area) {
+    switch (area) {
+        case "arch":
+            return "#FFE354";
+        case "refact":
+            return "#2D75D4";
+        case "verif":
+            return "#A74DE3";
+        case "req":
+            return "#E94646";
+        default:
+            return "#C4C4C4";
+    }
+}
+
+function getShadowColor(area) {
+    switch (area) {
+        case "arch":
+            return "rgba(255,227,84, 0.5)";
+        case "refact":
+            return "rgba(45,117,212, 0.5)";
+        case "verif":
+            return "rgba(167,77,227, 0.5)";
+        case "req":
+            return "rgba(233,70,70, 0.5)";
+        default:
+            return "#C4C4C4";
+    }
+}
+
 export default function Featured(props) {
     return (
-        <div className="card" style={{backgroundColor: "#EFEFEF", height: "225px", minWidth: "1fr", display: "flex", flexDirection: "column"}}>
-            <div className="header" style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "start", padding: "20px", paddingBottom: "10px"}}>
+        <div className="featured-card" style={{backgroundColor: "#EFEFEF", color: "black", borderRadius: "40px", height: "225px", minWidth: "1fr", display: "flex", flexDirection: "column", padding: "10px", boxShadow: "2px 4px 5px 0px " + getShadowColor(props.area)}}>
+            <div className="header" style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "start", padding: "20px 20px 10px 20px"}}>
                 <h4 className="card-title" style={{padding: "0px", margin: 0 + "px", marginRight: 5 + "px"}}>{props.name}</h4>
                 <p className="card-text" style={{padding: 0 + "px", color: "#C4C4C4"}}>by {props.teamId}</p>
             </div>
-            <div className="card-body" style={{padding: "20px", paddingTop: "0px", paddingBottom: "0px", margin: "0px"}}>
-                <p className="card-text" style={{textAlign: "justify"}}>{props.description}</p>
+            <div className="featured-body" style={{padding: "20px", paddingTop: "0px", paddingBottom: "0px", margin: "0px 0px 10px 0px"}}>
+                <p className="card-text" style={{textAlign: "justify", color: "#636363"}}>{props.description}</p>
             </div>
-            <Image src="/pictures/logo.svg" alt="Logo" height={60} width={"100"} />
-            <a className="navbar-brand d-flex align-items-center" href="/"></a>
+            <div className="featured-footer" style={{display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "end", padding: "10px 20px 10px 20px"}}>
+                <button className="btn btn-primary" type="button" style={{display: "flex", justifyContent: "center", alignContent: "center", height: "38px", width: "135px", border: "1.5px solid rgba(196,196,196, 0.75)", backgroundColor: getAccentColor(props.area), boxShadow: "0px 0px 10px 1px rgba(196,196,196,1)", color: "#000000", fontWeight: "600"}}>USE IT</button>
+            </div>
         </div>
 
     );
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -37,7 +37,7 @@ export default function Home() {
         </div>
 
         <div className="container" style={{display: "grid", padding: "20px 30px 20px 30px", gridTemplateColumns: "repeat(3, 1fr)", backgroundColor: "white", height: "400px", minWidth: "100%"}}>
-          <Featured name="Archy" teamId="1MEIC01T2" description="Analyze local data such as commit logs and  documentation in software repositories and infer which patterns are present in source code." />
+          <Featured name="Archy" teamId="1MEIC01T2" description="Analyze local data such as commit logs and  documentation in software repositories and infer which patterns are present in source code." area="arch" />
         </div>
         <footer className={styles.footer}>
         
