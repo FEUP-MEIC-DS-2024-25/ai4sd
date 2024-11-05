6bd3d17902c54d79503c48b1b2ded80ed45b5c49
Daniel Pinho <drp@fe.up.pt>
2024-11-05 15:38:04 +0000

    Implement an early version of the assistant picker

--- frontend/src/app/pages/interactor.js
+++ frontend/src/app/pages/interactor.js
@@ -5,11 +5,14 @@ import 'bootstrap/dist/css/bootstrap.css';
 import '../globals.css';
 
 import logo from "../pictures/logo.svg";
+import AssistantPicker from "../components/assistantPicker";
 
 export default function Home() {
     return(
         <div>
+            <AssistantPicker/>
             <h1>Interactor page!</h1>
+
         </div>
     )
 }
