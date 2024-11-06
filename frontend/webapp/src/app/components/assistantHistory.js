6ca16c0460520c9355700f942f808ebe541d473e
Daniel Pinho <drp@fe.up.pt>
2024-11-06 10:52:36 +0000

    Enhance history item presentation
    The history items now truncate when the text is too long and grow on hover.

--- frontend/webapp/src/app/components/assistantHistory.js
+++ frontend/webapp/src/app/components/assistantHistory.js
@@ -7,7 +7,7 @@ export default function AssistantHistory({ name, type, interactions }) {
     return (
         <div className={styles.assistantHistory}>
             <HistoryHeader name={name} type={type} />
-            <HistoryList interactions={interactions} />
+            <HistoryList interactions={interactions}  />
         </div>
 
     )
@@ -26,19 +26,21 @@ function HistoryHeader({ name, type }) {
 
 function HistoryList({ interactions }) {
     return (
-        <ul className="nav flex-column mb-auto text-left gap-2 px-3">
+        <ul className="nav flex-column mb-auto text-left gap-3 px-3 mt-3">
             {interactions.map((interaction, index) => (
-                <li className="nav-item" key={index} >
-                    <HistoryItem text={interaction.text} link={interaction.link}/>
+                <a href={interaction.link} style={{textDecoration:"none"}}>
+                <li className="nav-item w-100" key={index} >
+                    <HistoryItem text={interaction.text} />
                 </li>
+                </a>
             ))}
         </ul>
     )
 }
 
-function HistoryItem({ text, link }) {
+function HistoryItem({ text }) {
     return (
-        <div className={styles.historyItem} href={link}>
+        <div className={styles.historyItem}>
             {text}
         </div>
     )
---
+++ frontend/webapp/src/app/components/assistantMenu.js
@@ -0,0 +1,15 @@
+export default function AssistantMenu() {
+    return (
+        <div class="btn-group w-100 p-3" role="group" aria-label="Basic example">
+            <button type="button" class="btn btn-secondary">New...</button>
+            <button type="button" class="btn btn-secondary"></button>
+            <button type="button" class="btn btn-secondary"></button>
+            <button type="button" class="btn btn-secondary">Settings</button>
+        </div>
+
+
+
+    )
+
+
+}
