67f53c38d50363027729fceb305903233721e252
José Miguel Isidro <93340308+jmisidro@users.noreply.github.com>
2024-11-14 14:27:01 +0000

    added tailwindcss

--- frontend/webapp/src/app/components/AssistantInteraction.js
+++ frontend/webapp/src/app/components/AssistantInteraction.js
@@ -4,7 +4,7 @@ import styles from "@/app/page.module.css";
 export default function AssistantInteraction() {
     return (
         <div className={styles.assistantInteraction}>
-            <h2>Hello World!</h2>
+            <h2 className='text-3xl font-bold'>Hello World!</h2>
             <h4>The assistant interactions will take place in this component.</h4>
 
         </div>
