7bb3bb42092a1e08535970a2282def652128ed24
Carlos Eduardo Duarte <25884573+cosmicblackcat@users.noreply.github.com>
2024-11-15 23:26:57 +0000

    Add sample folders for testing

---
+++ jarvis/app.py
@@ -0,0 +1,17 @@
+import fastapi
+
+app = fastapi.FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+# Get target repository
+@app.get("/repos/{repository}")
+def read_item(repository: str):
+    return {"repository": repository}
+
+# Go to target assistant
+@app.get("/assistant/{assistant}")
+def read_item(assistant: str):
+    return {"assistant": assistant}
