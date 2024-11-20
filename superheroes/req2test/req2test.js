075b634bb45968976bdf254eed791e44e95623a5
José Miguel Isidro <93340308+jmisidro@users.noreply.github.com>
2024-11-20 15:17:13 +0000

    integrating Req2Test in avengers

---
+++ superheroes/req2test/req2test.js
@@ -0,0 +1,10 @@
+import styles from "../../avengers/frontend/webapp/src/app/page.module.css";
+
+export default function Req2Test() {
+    return (
+        <div className={styles.assistantInteraction}>
+            <h2 className='text-3xl font-bold'>Welcome to Req2Test</h2>
+            <p className='text-lg'>This is a test page for Req2Test.</p>
+        </div>
+    )
+}
