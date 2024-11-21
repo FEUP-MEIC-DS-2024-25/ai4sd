eec457d3eb5cac12fc7773952fbbbd38e8b8848c
Mª Rita Lopes <up202108819@up.pt>
2024-11-21 22:12:16 +0000

    Complete auth and refactor

---
+++ jarvis/jarvis-fetcher/githubClient.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export async function fetchRepoContents(octokit, org, repo, path = "") {
+    const { data } = await octokit.request("GET /repos/{owner}/{repo}/contents/{path}", {
+        owner: org,
+        repo,
+        path,
+    });
+    return data;
+}
+
+export async function downloadFile(url) {
+    const response = await axios.get(url);
+    return response.data;
+}
