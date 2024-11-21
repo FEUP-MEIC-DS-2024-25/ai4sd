381b7c2188a15febc44836d0b74893d874927a42
ricardo-peralta-0000 <ricardoperalta0000@gmail.com>
2024-11-21 17:01:10 +0000

    Add styling and structure to message input components

--- avengers/frontend/webapp/src/app/featurecraft/components/messageBlock.js
+++ avengers/frontend/webapp/src/app/featurecraft/components/messageBlock.js
@@ -1,3 +1,5 @@
+import styles from "@/app/page.module.css";
+
 export default function MessageBlock() {
     
 }
--- avengers/frontend/webapp/src/app/featurecraft/components/newMessageBlock.js
+++ avengers/frontend/webapp/src/app/featurecraft/components/newMessageBlock.js
@@ -1,3 +1,26 @@
+import styles from "@/app/page.module.css";
+
 export default function NewMessageBlock() {
-    
+    return(
+        <div className="flex flex-col justify-end h-full items-center">
+            <div className="min-h-20 w-full flex justify-center p-3 relative">
+                <div className="w-full flex">
+                    <textarea
+                        className="flex-grow p-2 border border-gray-300 rounded resize-none overflow-hidden min-h-16 max-h-40 pr-10 mr-2"
+                        placeholder="Type your message here..."
+                        rows="1"
+                        onInput={(e) => {
+                            e.target.style.height = 'auto';
+                            e.target.style.height = `${e.target.scrollHeight}px`;
+                        }}
+                    />
+                    <button className="w-16 flex justify-center items-center border bg-[#6C757D] rounded">
+                        <svg className="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M5 13V1m0 0L1 5m4-4 4 4" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
 }
