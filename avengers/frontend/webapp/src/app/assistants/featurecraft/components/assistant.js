624a3530c5efe3ed817f210050b06279604eae25
ricardo-peralta-0000 <up202206392@up.pt>
2024-11-22 22:10:18 +0000

    Add NewFeaturecraftAssistant component for handling default url and new messages

--- avengers/frontend/webapp/src/app/assistants/featurecraft/components/assistant.js
+++ avengers/frontend/webapp/src/app/assistants/featurecraft/components/assistant.js
@@ -75,3 +75,51 @@ export default function FeaturecraftAssistant({ conversationId, setConversationI
         </div>
     )
 }
+
+export function NewFeaturecraftAssistant({ conversationId, setConversationId }) {
+    const [members, setMembers] = useState(["You", "Gemini"]);
+    const [description, setDescription] = useState("Welcome to the Featurecraft Assistant!");
+    const [messages, setMessages] = useState([]);
+    const [pinnedMessages, setPinnedMessages] = useState([]);
+    const [totalMessages, setTotalMessages] = useState(0);
+
+    const handleSendMessage = (sentMessage) => {
+        setMessages((prevMessages) => [
+            ...prevMessages,
+            sentMessage.newMessage
+        ]);
+    };
+
+    const handleReceiveMessage = (response) => {
+        console.log(response.data);
+
+        if (response.status === 200) {
+            setMessages((prevMessages) => [
+                ...prevMessages,
+                response.data
+            ]);
+        } else if (response.status === 201) {
+            setConversationId(response.data._id);
+            setMembers(response.data.members);
+            setDescription(response.data.description);
+            const assistantResponse = response.data.messages[response.data.messages.length - 1];
+            setMessages((prevMessages) => [
+                ...prevMessages,
+                assistantResponse
+            ]);
+            setPinnedMessages(response.data.pinnedMessages);
+            setTotalMessages(response.data.totalMessages);
+        } else {
+            console.error(response);
+        }
+    };
+
+    return (
+        <div className={`${styles.assistantInteraction} w-full h-full`}>
+            <div className="w-full h-full rounded-lg shadow-lg flex flex-col">
+                <MessageBlock messages={messages} totalMessages={totalMessages} description={description} />
+                <NewMessageBlock onSendMessage={handleSendMessage} onReceiveMessage={handleReceiveMessage} conversationId={conversationId} />
+            </div>
+        </div>
+    )
+}
