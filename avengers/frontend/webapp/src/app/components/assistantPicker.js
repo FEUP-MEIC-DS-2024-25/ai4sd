921dcc04822be634fdcc0d6eab802fcffd7ce197
ricardo-peralta-0000 <up202206392@up.pt>
2024-11-22 15:50:09 +0000

    Migrated the entire assistant frontend to the correct folder

--- avengers/frontend/webapp/src/app/components/assistantPicker.js
+++ avengers/frontend/webapp/src/app/components/assistantPicker.js
@@ -20,7 +20,7 @@ function getAssistantList() {
     let ai3 = { name: "ai3", type: "verif", link: "#" };
     let ai4 = { name: "ai4", type: "req", link: "#" };
     let ai5 = { name: "ai5", type: "arch", link: "#" };
-    let ai6 = { name: "FeatureCraft", type: "req", link: "featurecraft" };
+    let ai6 = { name: "FeatureCraft", type: "req", link: "/assistants/featurecraft" };
 
     return [ai1, ai2, ai3, ai4, ai5, ai6]
 }
