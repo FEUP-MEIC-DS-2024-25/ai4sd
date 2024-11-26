0b989fe371ada4ef611a3018cd631ec46fb57e55
Daniel Nunes <132585776+daniel-nunes-03@users.noreply.github.com>
2024-11-26 02:10:16 +0000

    Correct integration of the frontend, backend missing

--- avengers/frontend/webapp/src/app/assistants/patternpartner/components/assistant.js
+++ avengers/frontend/webapp/src/app/assistants/patternpartner/components/assistant.js
@@ -1,4 +1,6 @@
 import styles from "@/app/page.module.css";
+import App from './app.js';
+
 export default function Assistant() {
     return (
         <div className={styles.assistantInteraction}>
