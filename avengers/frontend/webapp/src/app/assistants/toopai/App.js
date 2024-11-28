43b7a3b6d265e7102411ca741a86aeb7d97f963e
rodrigofeup <up202403040@edu.fe.up.pt>
2024-11-28 17:24:01 +0000

    Create App.js

---
+++ avengers/frontend/webapp/src/app/assistants/toopai/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ChatArea from "./ChatArea";
+
+function App() {
+  return (
+    <div className="App">
+      <ChatArea />
+    </div>
+  );
+}
+
+export default App;
