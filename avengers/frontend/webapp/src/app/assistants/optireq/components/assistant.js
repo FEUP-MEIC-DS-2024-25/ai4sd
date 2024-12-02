c327932d4289c79e815554a2d1b897e2fce0f0ca
FranciscoS0usa <fgoncalves032@gmail.com>
2024-12-02 17:38:48 +0000

    added simple optireq directory to frontend

---
+++ avengers/frontend/webapp/src/app/assistants/optireq/components/assistant.js
@@ -0,0 +1,9 @@
+import styles from "@/app/page.module.css";
+export default function Assistant() {
+    return (
+        <div className={styles.assistantInteraction}>
+            <h1 className="text-3xl font-bold text-indigo-600">OptiReq</h1>
+            <p>Example component for OptiReq</p>
+        </div>
+    )
+}
