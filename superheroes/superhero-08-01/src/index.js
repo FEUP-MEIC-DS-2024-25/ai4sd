218c4a674b06b7f0ddad551b69e4ce1a74192415
Tomás Palma <up202108880@edu.fe.up.pt>
2024-12-02 22:39:25 +0000

    feat: dockerfile can receive external secrets

--- superheroes/superhero-08-01/src/index.js
+++ superheroes/superhero-08-01/src/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import { setupRoutes } from './api/index.js';
+import { fileURLToPath } from 'url';
 import 'dotenv/config';
 import path from 'path';
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, 'public')));
 
 const port = process.env.PORT || 3000;
