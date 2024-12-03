d7a0a39ee7c82a10740d6483e9f2ca1bed7dcd41
FranciscoCardoso913 <franciscocardoso.3003@gmail.com>
2024-12-03 00:04:12 +0000

    Making text input

--- avengers/frontend/webapp/src/app/assistants/gerald/components/assistant.js
+++ avengers/frontend/webapp/src/app/assistants/gerald/components/assistant.js
@@ -1,13 +1,25 @@
 import styles from "@/app/page.module.css";
 import "./gerald.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 export default function Assistant() {
     return (
         <div className={`${styles.assistantInteraction} assistantInteraction`}>
             <h1 className="">gerald.</h1>
             <p className="">not your average assistant</p>
-            <div className="input">
+            <form className="input">
+                <input type="file"  id="fileInput" ></input>
+                <label for="fileInput" class="custom-file-label file">
+                    <i class="fas fa-paperclip"></i>
+                </label>
+                
                 <input type="text"></input>
-            </div>
+
+                <input type="submit"  id="submit" ></input>
+                <label for="submit" class="custom-file-label">
+                    <i className="fas fa-paper-plane"></i> 
+                </label>
+            </form>
         </div>
     )
 }
