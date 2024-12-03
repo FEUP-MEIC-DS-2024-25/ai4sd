6c0579da127c0ae68805a67e149041159e0b066b
FranciscoCardoso913 <franciscocardoso.3003@gmail.com>
2024-12-03 15:34:14 +0000

    Messages components

--- avengers/frontend/webapp/src/app/assistants/gerald/components/assistant.js
+++ avengers/frontend/webapp/src/app/assistants/gerald/components/assistant.js
@@ -1,34 +1,34 @@
+'use client';
 import styles from "@/app/page.module.css";
 import "./gerald.css";
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import AssistantChat from "./chat";
+import React, { useState } from "react";
 
 export default function Assistant() {
+    const [hasMsg, setHasMsg] = useState(false); 
+
+    const checkMsg = ()=>{
+        setHasMsg(true)
+    }
     return (
         <div className={`${styles.assistantInteraction} assistantInteraction`}>
             <header>
                 <a href="">
-                    <h2><i class="fas fa-sync-alt"></i> Change to another Project</h2>
+                    <h2><i className="fas fa-sync-alt"></i> Change to another Project</h2>
                 </a>
                 <a className="aboutus" href="">
                     <h2>About us</h2>
                 </a>
                 </header>
             <div className="main">
-                <h1 className="">gerald.</h1>
-                <p className="">not your average assistant</p>
-                <form className="input">
-                    <input type="file"  id="fileInput" ></input>
-                    <label for="fileInput" class="custom-file-label file">
-                        <i class="fas fa-paperclip"></i>
-                    </label>
-                    
-                    <input type="text"></input>
-
-                    <input type="submit"  id="submit" ></input>
-                    <label for="submit" class="custom-file-label">
-                        <i className="fas fa-paper-plane"></i> 
-                    </label>
-                </form>
+            {!hasMsg ?(
+                <div>
+                    <h1 className="">gerald.</h1>
+                    <p className="">not your average assistant</p>
+                </div>
+            ):(<></>)}
+                <AssistantChat callback={checkMsg}></AssistantChat>
             </div>
         </div>
     )
---
+++ avengers/frontend/webapp/src/app/assistants/gerald/components/chat.js
@@ -0,0 +1,39 @@
+import React, { useState, useRef, useEffect } from "react";
+import AssistantForm from "./form.js";
+import { sendMsg } from "../services/gerald";
+import Message from "./message.js"
+
+export default function AssistantChat(props) {
+    const [messages, setMessages] = useState([]); // State to store the list of messages
+    const messagesEndRef = useRef(null); // Ref for the end of the messages
+    const scrollToBottom = () => {
+        if (messagesEndRef.current) {
+            messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+        }
+    };
+
+    const handleCallback = (newMessage) => {
+        props.callback()
+        setMessages((prevMessages) => [...prevMessages, newMessage]); // Add the new message to the list
+        
+        sendMsg(newMessage).then((response)=>{ setMessages((prevMessages) => [...prevMessages, response]);})
+    };
+
+    useEffect(() => {
+        scrollToBottom();
+    }, [messages]);
+
+    return (
+        <div>
+            <div className="scrollable">
+                <div >
+                    {messages.map((message, index) => (
+                        <Message index={index}>{message}</Message> // Render each message as a <p>
+                    ))}
+                </div>
+                <div ref={messagesEndRef} /> 
+            </div>
+            <AssistantForm callback={handleCallback} />
+        </div>
+    );
+}
