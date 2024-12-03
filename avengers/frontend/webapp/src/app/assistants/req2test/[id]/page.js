1cd8269ac5066807bed2b8c762c3d0ffa3f7be9c
José Miguel Isidro <93340308+jmisidro@users.noreply.github.com>
2024-12-03 10:25:57 +0000

    integrating assistant

--- avengers/frontend/webapp/src/app/assistants/req2test/[id]/page.js
+++ avengers/frontend/webapp/src/app/assistants/req2test/[id]/page.js
@@ -9,13 +9,12 @@ import '@/app/globals.css';
 
 import AssistantPicker from "@/app/components/assistantPicker";
 import AssistantHistory from "@/app/components/assistantHistory";
-import Assistant from "./components/assistant"
-import ChatNotFound from "./components/chatNotFound";
+import Assistant from "../components/assistant"
+import ChatNotFound from "../components/chatNotFound";
 
 
 export default function Interactor() {
-    // Retrieve the id from the local storage
-    const id = localStorage.getItem("req2testId") ? parseInt(localStorage.getItem("req2testId")) : 1;
+    const { id } = useParams();
     const assistName = "Req2Test";
     const assistType = "req";
     const [assistHistory, setAssistHistory] = useState(prepareMockHistory);
@@ -25,27 +24,27 @@ export default function Interactor() {
     const mockChats = [
         { id: 1, name: "Chat 1", 
             messages: [
-                { content: "1Welcome! I am here to help you convert your requirements into Gherkin tests. How can I assist you today?", sender: "bot" },
+                { content: "Welcome! I am here to help you convert your requirements into Gherkin tests. How can I assist you today?", sender: "bot" },
                 { content: "I need help with a requirement", sender: "user" },
                 { content: "Sure! Please paste your requirement here", sender: "bot" },
             ] 
         },
         { id: 2, name: "Chat 2", 
             messages: [
-                { content: "2Welcome! I am here to help you convert your requirements into Gherkin tests. How can I assist you today?", sender: "bot" },
+                { content: "Welcome! I am here to help you convert your requirements into Gherkin tests. How can I assist you today?", sender: "bot" },
                 { content: "I need help with a requirement", sender: "user" },
                 { content: "Sure! Please paste your requirement here", sender: "bot" },
             ] 
         },
         { id: 3, name: "Chat 3", 
             messages: [
-                { content: "3Welcome! I am here to help you convert your requirements into Gherkin tests. How can I assist you today?", sender: "bot" },
+                { content: "Welcome! I am here to help you convert your requirements into Gherkin tests. How can I assist you today?", sender: "bot" },
                 { content: "I need help with a requirement", sender: "user" },
                 { content: "Sure! Please paste your requirement here", sender: "bot" },
             ] 
         }
     ]
-    
+
 
     useEffect(() => {
         if (id > 3) {
@@ -55,6 +54,9 @@ export default function Interactor() {
             setChat(mockChats[id - 1]);
             setChatExists(true);
         }
+
+        // Save id to local storage
+        localStorage.setItem("req2testId", id);
     }, [id]);
 
 
