57638c6bf9be669a1e31cccef4d64a2a8d66e2e2
Joao Padrao <up202108766@up.pt>
2024-12-03 17:46:22 +0000

    Created Miro Board Template API

--- superheroes/superhero-08-02/spark/api/services/miro.py
+++ superheroes/superhero-08-02/spark/api/services/miro.py
@@ -1,4 +1,10 @@
 import os, random, requests, re
+from github_graphql import GitHubGraphQLAPI
+from gemini import GeminiAPI
+
+import json
+from dotenv import load_dotenv
+load_dotenv()
 
 class MiroAPI:
 
@@ -100,7 +106,7 @@ class MiroAPI:
 
 
     def create_priority_tag(self, board_id, content):
-        url = f"{self.BASE_URL}/boards/{board_id}/tags"
+        url = f"{self.BASE_URL}/{board_id}/tags"
 
         color = None
         if content == "High":
@@ -120,7 +126,7 @@ class MiroAPI:
 
 
     def create_size_tag(self, board_id, size):
-        url = f"{self.BASE_URL}/boards/{board_id}/tags"
+        url = f"{self.BASE_URL}/{board_id}/tags"
 
         size = int(size)
         color = None
@@ -146,7 +152,7 @@ class MiroAPI:
 
     # Return a dictionary with the tags of the board and their respective IDs
     def get_tag_id(self, board_id):
-        url = f"{self.BASE_URL}/boards/{board_id}/tags"
+        url = f"{self.BASE_URL}/{board_id}/tags"
 
         headers = {
             "accept": "application/json",
@@ -164,7 +170,7 @@ class MiroAPI:
     # Return dictionary with the sticky notes of the board and their respective IDs
     def get_sticky_notes_id(self, board_id):
 
-        url = f"{self.BASE_URL}/boards/{board_id}/items"
+        url = f"{self.BASE_URL}/{board_id}/items?limit=50"
 
         headers = {
             "accept": "application/json",
@@ -183,7 +189,7 @@ class MiroAPI:
 
 
     def attach_note_to_tag(self, board_id,  note_id, tag_id):
-        url = f"{self.BASE_URL}/boards/{board_id}/items/{note_id}?tag_id={tag_id}"
+        url = f"{self.BASE_URL}/{board_id}/items/{note_id}?tag_id={tag_id}"
 
         headers = {
             "accept": "application/json",
@@ -194,7 +200,7 @@ class MiroAPI:
         return response
 
 
-    def extract_lists_from_response(response):
+    def extract_lists_from_response(self,response):
         matches = re.findall(r'\[([^\]]*?)\]', response)
         
         if matches and len(matches) >= 3:
@@ -212,7 +218,7 @@ class MiroAPI:
     def update_sticky_note(self, board_id, note_id):
         width = 1000
         x = random.randint(width/2, 3500 - width/2)
-        url = f"{self.BASE_URL}/boards/{board_id}/sticky_notes/{note_id}"
+        url = f"{self.BASE_URL}/{board_id}/sticky_notes/{note_id}"
 
         payload = { "position": { "x": x } }
 
@@ -221,7 +227,7 @@ class MiroAPI:
 
 
     def get_iteration_tasks(self, board_id, sticky_notes_dict):
-        url = f"{self.BASE_URL}/boards/{board_id}/items"
+        url = f"{self.BASE_URL}/{board_id}/items"
 
         headers = {
             "accept": "application/json",
@@ -242,7 +248,7 @@ class MiroAPI:
         
         for task in iteration_tasks:
             task_id = sticky_notes_dict[task]
-            tags_info = self.get_tags_from_notes(task_id)
+            tags_info = self.get_tags_from_notes("uXjVLQTokqg%3D", task_id)
             priority_tasks.append(tags_info["priority_tag"])
             size_tasks.append(tags_info["size_tag"])
 
@@ -251,7 +257,7 @@ class MiroAPI:
 
     def get_tags_from_notes(self, board_id, note_id):
 
-        url = f"{self.BASE_URL}/boards/{board_id}/items/{note_id}/tags"
+        url = f"{self.BASE_URL}/{board_id}/items/{note_id}/tags"
 
         headers = {
             "accept": "application/json",
@@ -275,8 +281,298 @@ class MiroAPI:
             "size_tag": size_tag
         }
     
+    def create_miro_template(self, board_id):
+        url_rect = f"{self.BASE_URL}/{board_id}/shapes"
+
+        url_text = f"{self.BASE_URL}/{board_id}/texts"
+
+        headers = {
+            "accept": "application/json",
+            "content-type": "application/json",
+            "authorization": f"Bearer {self.bearer_token}"
+        }
+
+        left_rectangle_payload = {
+            "data": { "shape": "rectangle" },
+            "style": {
+                "borderOpacity": "1",
+                "borderWidth": "12"
+            },
+            "geometry": {
+                "height": 4430,
+                "width": 3850
+            },
+            "position": {
+                "x": -1950,
+                "y": 330
+            }
+        }
+        
+        right_rectangle_payload = {
+            "data": { "shape": "rectangle" },
+            "style": {
+                "borderOpacity": "1",
+                "borderWidth": "12"
+            },
+            "geometry": {
+                "height": 4430,
+                "width": 3850
+            },
+            "position": {
+                "x": 1900,
+                "y": 330
+            }
+        }
+
+        min_left_rectangle_payload = {
+            "data": { "shape": "rectangle" },
+            "style": {
+                "borderOpacity": "1",
+                "borderWidth": "12"
+            },
+            "geometry": {
+                "height": 700,
+                "width": 3850
+            },
+            "position": {
+                "x": -1950,
+                "y": -2220
+            }
+        }
+
+        min_right_rectangle_payload = {
+            "data": { "shape": "rectangle" },
+            "style": {
+                "borderOpacity": "1",
+                "borderWidth": "12"
+            },
+            "geometry": {
+                "height": 700,
+                "width": 3850
+            },
+            "position": {
+                "x": 1900,
+                "y": -2220
+            }
+        }
+
+        top_rectangle_payload = {
+            "data": { "shape": "rectangle" },
+            "style": {
+                "borderOpacity": "1",
+                "borderWidth": "12",
+                "fillColor": "#FEF445",
+                "borderColor": "#FAC710"
+            },
+            "geometry": {
+                "height": 650,
+                "width": 7700
+            },
+            "position": {
+                "x": 0,
+                "y":-3100
+            }
+        }
+
+        miro_text_payload = {
+            "data": { "content": "Miro's Board - SPARK" },
+            "style": {
+                "fontFamily": "fredoka_one",
+                "fontSize": "288"
+            },
+            "geometry": { "width": 4000 },
+            "position": {
+                "y": -3100,
+                "x": -700
+            }
+        }
+
+        # Product Backlog
+        miro_pb_text_payload = {
+            "data": { "content": "📋 <strong>Product Backlog </strong>" },
+            "style": {
+                "fontFamily": "open_sans",
+                "fontSize": "200",
+                "fillOpacity": "1"
+            },
+            "geometry": { "width": 4000 },
+            "position": {
+                "y": -2200,
+                "x": -910
+            }
+        }
+
+        # Iteration Backlog
+        miro_ib_text_payload = {
+            "data": { "content": "🔖 <strong>Iteration Backlog </strong>" },
+            "style": {
+                "fontFamily": "open_sans",
+                "fontSize": "200",
+                "fillOpacity": "1"
+            },
+            "geometry": { "width": 4000 },
+            "position": {
+                "y": -2200,
+                "x": 2900
+            }
+        }
+
+        requests.post(url_rect, json=left_rectangle_payload, headers=headers)
+
+        requests.post(url_rect, json=right_rectangle_payload, headers=headers)
+
+        requests.post(url_rect, json=min_left_rectangle_payload, headers=headers)       
+    
+        requests.post(url_rect, json=min_right_rectangle_payload, headers=headers)
+
+        requests.post(url_rect, json=top_rectangle_payload, headers=headers)
+
+        requests.post(url_text, json=miro_text_payload, headers=headers)
+
+        requests.post(url_text, json=miro_pb_text_payload, headers=headers)
+
+        requests.post(url_text, json=miro_ib_text_payload, headers=headers)
+
 def main():
-    miroAPI = MiroAPI()
+    #------TEST FUNCTIONS------
+
+    # 1. Get Project data from github 
+    GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
+    github_api = GitHubGraphQLAPI(GITHUB_TOKEN)
+
+     # Query to get the project data
+    query = """
+    {
+      node(id: "PVT_kwDOCtw04M4Ap0aW") {
+        ... on ProjectV2 {
+          items(first: 20) {
+            nodes {
+              id  
+              fieldValues(first: 8) {
+                nodes {
+                  ... on ProjectV2ItemFieldTextValue {
+                    text
+                    field {
+                      ... on ProjectV2FieldCommon {
+                        name
+                      }
+                    }
+                  }
+                  ... on ProjectV2ItemFieldDateValue {
+                    date
+                    field {
+                      ... on ProjectV2FieldCommon {
+                        name
+                      }
+                    }
+                  }
+                  ... on ProjectV2ItemFieldSingleSelectValue {
+                    name
+                    field {
+                      ... on ProjectV2FieldCommon {
+                        name
+                      }
+                    }
+                  }
+                }
+              }
+              content {
+                ... on DraftIssue {
+                  title
+                  body
+                }
+                ... on Issue {
+                  title
+                  assignees(first: 10) {
+                    nodes {
+                      login
+                    }
+                  }
+                }
+                ... on PullRequest {
+                  title
+                  assignees(first: 10) {
+                    nodes {
+                      login
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    """
+
+    # Send the request to the API to get the project data
+    project_data = github_api.send_request(query)
+    print("PROJECT DATA:", project_data)
+
+    # 2. Gemini choose tasks to move to the 'Iteration Backlog'
+
+    geminiAPI = GeminiAPI()
+
+    prompt = (
+        "Based on the provided project data, " + project_data + """\n
+        Identify the tasks that should be moved to the 'Iteration Backlog' for the upcoming iteration, prioritizing tasks with the highest impact.
+        Leave tasks not selected in their current status and do not modify their position.
+        Provide the response in a Python list with the names of all the tasks in the backlog. 
+        Additionally, provide two more Python lists with the priorities of the tasks and their sizes for all the tasks in the backlog in the same order.
+        Finally, provide one more Python list with the names of the tasks that should be moved to the 'Iteration Backlog'.
+        Don't give an explanation, just the lists.
+        """
+    )
+    
+    gemini_response = geminiAPI.prompt_gemini(prompt)
+
+    # 3. Create sticky notes in Miro for the tasks that should be moved to the 'Iteration Backlog'
+    print("Extracting lists from gemini response...")
+    MIRO_TOKEN = os.getenv('MIRO_TOKEN')    
+    miro_api = MiroAPI(MIRO_TOKEN)
+
+    print("Creating Miro template...")
+    miro_api.create_miro_template("uXjVLQTokqg%3D")
+
+    lists = miro_api.extract_lists_from_response(gemini_response)
+    all_tasks = lists[0]
+    priorities = lists[1]
+    sizes = lists[2]
+    iteration_tasks = lists[3]
+
+    print("Creating sticky notes...")
+    for i in range(len(all_tasks)):
+        miro_api.create_sticky_note("uXjVLQTokqg%3D", all_tasks[i])
+
+    print("Creating priority tags...")
+    for i in range(len(priorities)):
+        miro_api.create_priority_tag("uXjVLQTokqg%3D", priorities[i])
+
+    print("Creating size tags...")
+    for i in range(len(sizes)):
+        miro_api.create_size_tag("uXjVLQTokqg%3D", sizes[i])
+
+    print("Attaching notes to tags...")
+    tags_dict = miro_api.get_tag_id("uXjVLQTokqg%3D")
+    sticky_notes_dict = miro_api.get_sticky_notes_id("uXjVLQTokqg%3D")
+
+    for note in all_tasks:
+        note_id = sticky_notes_dict[note]
+        priority_id = tags_dict[priorities[all_tasks.index(note)]]
+        size_id = tags_dict[sizes[all_tasks.index(note)]]
+
+        miro_api.attach_note_to_tag("uXjVLQTokqg%3D", note_id, priority_id)
+        miro_api.attach_note_to_tag("uXjVLQTokqg%3D", note_id, size_id)
+
+    print("Updating sticky notes...")
+    for note in iteration_tasks:
+        note_id = sticky_notes_dict[note]
+        miro_api.update_sticky_note("uXjVLQTokqg%3D", note_id)
+
+    print("Miro is updated with the tasks for the upcoming iteration successfully!")
+
+    # Get iteration tasks from Miro
+    sticky_notes_dict = miro_api.get_sticky_notes_id("uXjVLQTokqg%3D")    
 
 if __name__ == "__main__":
     main()
