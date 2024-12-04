4f51d532f54c339f70c0a081b6d3cf07c4d21ee6
Tomás Ramos <tomaspr123@gmail.com>
2024-12-04 14:07:54 +0000

    started the assistant page

---
+++ avengers/frontend/webapp/src/app/AI_assistant/mainpage.tsx
@@ -0,0 +1,25 @@
+import styles from "@/app/page.module.css";
+import 'bootstrap/dist/css/bootstrap.css';
+
+import '@/app/globals.css';
+
+import Footer from "@/app/components/footer";
+import NavBar from "@/app/components/navbar";
+import { ChatBubble } from "./components/chat/chat-bubble";
+
+
+
+export default function AI_assistant() {
+
+    const assistName = "AI_assitant"
+    const assitType = "verif"
+
+    return (
+        <main className={styles.main} style={{ color: "none" }}>
+            <NavBar />
+            <ChatBubble />
+            <Footer />
+        </main>
+    );
+
+}
