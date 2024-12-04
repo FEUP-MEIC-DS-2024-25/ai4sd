78d68d3f6a7301a5c032d2c23a0cb33cf9a6b64a
Henrique Silva <hggfsilva@gmail.com>
2024-12-04 16:52:53 +0000

    fix: fixed need for double click to submit extension options

--- x-men/frontend/public/js/menu.js
+++ x-men/frontend/public/js/menu.js
@@ -3,8 +3,5 @@ const vscode = acquireVsCodeApi();
 document.getElementById('submit').addEventListener('click', () => {
     const loc = document.getElementById('location');
     const locOption = loc.options[loc.selectedIndex].innerHTML;
-
-    document.getElementById('submit').addEventListener('click', () => {
-        vscode.postMessage({ command: 'location', text: locOption });
-    });
+    vscode.postMessage({ command: 'location', text: locOption });
 });
