57183451b01b9daa7e3985739259529e1b22a4f6
Simoes <pedrosimoes.ps@hotmail.com>
2024-12-04 20:00:59 +0000

    Adding register to general backend

--- superheroes/superhero-07-01/app.py
+++ superheroes/superhero-07-01/app.py
@@ -8,22 +8,23 @@ app = FastAPI()
 class UserStoryRequest(BaseModel):
     user_story: str
 
-@app.post('/api/gemini/{conversation_id}')
-def generate_answer(conversation_id: str, request: UserStoryRequest):    
-    if check_if_user_story_format(request.user_story):
-        with open("requests.json", "r") as file:
-            gemini_prompt = json.load(file)
+def register_story2test_api(app: FastAPI):
+    @app.post('/api/gemini/{conversation_id}')
+    def generate_answer(conversation_id: str, request: UserStoryRequest):    
+        if check_if_user_story_format(request.user_story):
+            with open("requests.json", "r") as file:
+                gemini_prompt = json.load(file)
         
-        full_prompt = request.user_story + " " + gemini_prompt["gemini_prompt"]
+            full_prompt = request.user_story + " " + gemini_prompt["gemini_prompt"]
 
-        try:
-            answer = call_gemini_api(full_prompt)
-            return {
-                "message": "User story format is correct. ",
-                "prompt": full_prompt,
-                "answer": answer.text
-            }
-        except Exception as e:
-            return JsonResponse({"error": str(e)}, status=500)
-    else:
-        return {"message": "User story format is incorrect."}
+            try:
+                answer = call_gemini_api(full_prompt)
+                return {
+                    "message": "User story format is correct. ",
+                    "prompt": full_prompt,
+                    "answer": answer.text
+                }
+            except Exception as e:
+                return JsonResponse({"error": str(e)}, status=500)
+        else:
+            return {"message": "User story format is incorrect."}
