234c7b09e5e867ffc950ff83bf25c02d0444d4a8
lessthelonely <42583814+lessthelonely@users.noreply.github.com>
2024-12-05 20:44:43 +0000

    Revert changes in TerraForm.

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -612,14 +612,12 @@ resource "google_project_iam_member" "strange_firestore_permissions" {
 #################################
 ### SECRETS #####################
 #################################
-resource "google_secret_manager_secret" "superhero_secrets" {
-  for_each = toset(flatten([for hero in local.superhero_names : [
-    "${hero}-secret-1",
-    "${hero}-secret-2",
-    "${hero}-secret-3"
-  ]]))
 
-  secret_id = each.key
+
+resource "google_secret_manager_secret" "superhero_secrets" {
+  for_each = toset(local.superhero_names)
+
+  secret_id = "${each.key}-secret"
   replication {
     user_managed {
       replicas {
@@ -627,22 +625,6 @@ resource "google_secret_manager_secret" "superhero_secrets" {
       }
     }
   }
-}
-
-resource "google_secret_manager_secret_iam_member" "cloud_build_secret_admin" {
-  for_each = google_secret_manager_secret.superhero_secrets
-
-  secret_id = google_secret_manager_secret.superhero_secrets[each.key].id
-  role      = "roles/secretmanager.admin"
-  member    = "serviceAccount:${google_service_account.cloud_build_sa.email}"
-}
-
-resource "google_secret_manager_secret_iam_member" "cloud_build_sa_secret_admin" {
-  for_each = google_secret_manager_secret.superhero_secrets
-
-  secret_id = google_secret_manager_secret.superhero_secrets[each.key].id
-  role      = "roles/secretmanager.admin"
-  member    = "serviceAccount:${google_service_account.cloud_build_sa.email}"
 }
 
 resource "google_secret_manager_secret_iam_member" "superhero_secret_access" {
@@ -658,19 +640,12 @@ resource "google_secret_manager_secret_iam_member" "superhero_secret_version" {
 
   secret_id = google_secret_manager_secret.superhero_secrets[each.key].id
   role      = "roles/secretmanager.SecretVersionManager"
-
-  # Extract the superhero key to match the service account map
-  member    = "serviceAccount:${google_service_account.superhero[split("-", each.key)[0]].email}"
+  member    = "serviceAccount:${google_service_account.superhero[each.key].email}"
 }
 
-resource "google_secret_manager_secret" "jarvis_secrets" {
-  for_each = {
-    secret1 = "jarvis-secret-1"
-    secret2 = "jarvis-secret-2"
-    secret3 = "jarvis-secret-3"
-  }
 
-  secret_id = each.value
+resource "google_secret_manager_secret" "jarvis_secret" {
+  secret_id = "jarvis-secret"
   replication {
     user_managed {
       replicas {
@@ -678,53 +653,16 @@ resource "google_secret_manager_secret" "jarvis_secrets" {
       }
     }
   }
-}
-
-resource "google_secret_manager_secret_iam_member" "jarvis_secret_version_manager" {
-  for_each = google_secret_manager_secret.superhero_secrets
-
-  secret_id = google_secret_manager_secret.superhero_secrets[each.key].id
-  role      = "roles/secretmanager.secretVersionManager"
-  member    = "serviceAccount:${google_service_account.jarvis.email}"
 }
 
 resource "google_secret_manager_secret_iam_member" "jarvis_secret_access" {
-  for_each = google_secret_manager_secret.jarvis_secrets
-
-  secret_id = each.value.id
+  secret_id = google_secret_manager_secret.jarvis_secret.id
   role      = "roles/secretmanager.secretAccessor"
-  member    = "serviceAccount:example-sa@project-id.iam.gserviceaccount.com"
-}
-
-resource "google_secret_manager_secret_iam_member" "jarvis_secret_version" {
-  for_each = google_secret_manager_secret.jarvis_secrets
-
-  secret_id = each.value.id
-  role      = "roles/secretmanager.secretVersionManager"
   member    = "serviceAccount:${google_service_account.jarvis.email}"
 }
 
-resource "google_secret_manager_secret" "strange_secrets" {
-  for_each = {
-    secret1 = "strange-secret-1"
-    secret2 = "strange-secret-2"
-    secret3 = "strange-secret-3"
-  }
-
-  secret_id = each.value
-  replication {
-    user_managed {
-      replicas {
-        location = "europe-west1"
-      }
-    }
-  }
-}
-
-resource "google_secret_manager_secret_iam_member" "strange_secret_version_manager" {
-  for_each = google_secret_manager_secret.strange_secrets
-
-  secret_id = google_secret_manager_secret.strange_secrets[each.key].id
-  role      = "roles/secretmanager.secretVersionManager"
-  member    = "serviceAccount:${google_service_account.strange.email}"
+resource "google_secret_manager_secret_iam_member" "jarvis_secret_version" {
+  secret_id = google_secret_manager_secret.jarvis_secret.id
+  role      = "roles/secretmanager.SecretVersionManager"
+  member    = "serviceAccount:${google_service_account.jarvis.email}"
 }
