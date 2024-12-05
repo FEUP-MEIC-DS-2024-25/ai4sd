709a95369f9a69033302f2a971b9c55fd0055c31
Tomás Gaspar <tomasc.gaspar@hotmail.com>
2024-12-05 21:56:38 +0000

    small restructure in extension

--- x-men/frontend/src/extension.ts
+++ x-men/frontend/src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as whattheduck from './superheroes/whattheduck/src/extension.js';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -47,6 +48,8 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+
+	whattheduck.activate(context);
 }
 
 // This method is called when your extension is deactivated
