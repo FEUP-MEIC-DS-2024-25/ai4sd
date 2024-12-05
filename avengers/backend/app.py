db48736ac8079e90a3ac33ebd1faa9024bbba288
Pedro <up202008467@edu.fe.up.pt>
2024-12-05 22:31:29 +0000

    backend joined in

--- avengers/backend/app.py
+++ avengers/backend/app.py
@@ -1,7 +1,11 @@
 import fastapi
-from superheroes.req2test.api import register_req2test_api
+# from superheroes.req2test.api import register_req2test_api
+from assistants.uta.api import register_uta_api
 
 app = fastapi.FastAPI()
+
+# register_req2test_api(app)
+register_uta_api(app)
 
 @app.get("/")
 def read_root():
@@ -16,5 +20,3 @@ def read_item(repository: str):
 @app.get("/assistant/{assistant}")
 def read_item(assistant: str):
     return {"assistant": assistant}
-
-register_req2test_api(app)
