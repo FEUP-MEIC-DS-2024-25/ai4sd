36a5e31043659eb5af4e10be04bc658a82fb7895
Rodrigo Campos Rodrigues <rodrigues.rodrigo2003@gmail.com>
2024-12-05 09:57:40 +0000

    refactor: :recycle: changed format of the data coming from the backend.

--- avengers/frontend/webapp/src/app/assistants/req2test/components/api.js
+++ avengers/frontend/webapp/src/app/assistants/req2test/components/api.js
@@ -5,6 +5,8 @@ const BASE_URL = 'http://localhost:8000';
 export const getChats = async () => {
     const response = await axios.get(`${BASE_URL}/get_chats`);
 
+    console.log(response.data);
+
     const chats = []
 
     for(let i = 0; i < response.data.length; i++) {
@@ -13,8 +15,10 @@ export const getChats = async () => {
 
         for(let j = 0; j < prompts.length; j++) {
 
-            let promptId = prompts[j].reference_id;
+            let promptId = prompts[j].id;
             let prompt = await axios.get(`${BASE_URL}/get_prompt/${promptId}`);
+
+            console.log("Prompt data:", prompt.data);
 
             const promptMessage = {
                 content: prompt.data.user_input,
@@ -23,9 +27,11 @@ export const getChats = async () => {
 
             messages.push(promptMessage);
 
-            let aiResponseId = prompt.data.response.reference_id
+            let aiResponseId = prompt.data.response.id
             let aiResponse = await axios.get(`${BASE_URL}/get_response/${aiResponseId}`);
             
+            console.log("Ai response data:", aiResponse.data);
+
             const aiResponseMessage = {
                 content: aiResponse.data.ai_response,
                 sender: "bot"
