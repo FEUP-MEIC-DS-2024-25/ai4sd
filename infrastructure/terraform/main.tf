102d89b6ed0cc195e890ffd441cde4f2cfae82fc
José Francisco Veiga <veigajosefrancisco@gmail.com>
2024-12-07 17:47:23 +0000

    Outputs for debugging and comment cloudbuild step 12

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -697,3 +697,19 @@ resource "google_secret_manager_secret_iam_member" "jarvis_secret_version" {
   role      = "roles/secretmanager.SecretVersionManager"
   member    = "serviceAccount:${google_service_account.jarvis.email}"
 }
+
+output "superhero_secret_ids" {
+  value = [for name in local.superhero_names : "${name}-secret"]
+}
+
+output "created_superhero_secrets" {
+  value = google_secret_manager_secret.superhero_secrets
+}
+
+output "superhero_secret_versions" {
+  value = google_secret_manager_secret_version.superhero_secrets
+}
+
+output "superhero_secret_access" {
+  value = google_secret_manager_secret_iam_member.superhero_secret_access
+}
