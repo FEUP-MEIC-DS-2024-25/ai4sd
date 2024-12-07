d349390819a4100508d2313277cd64cf071ece0c
José Francisco Veiga <veigajosefrancisco@gmail.com>
2024-12-07 12:38:39 +0000

    Add .id for jarvis_secret resource

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -671,7 +671,7 @@ resource "google_secret_manager_secret" "jarvis_secret" {
 }
 
 resource "google_secret_manager_secret_version" "jarvis_secret" {
-  secret = google_secret_manager_secret.jarvis_secret
+  secret = google_secret_manager_secret.jarvis_secret.id
   secret_data = "initial-secret-data for jarvis"
 }
 
