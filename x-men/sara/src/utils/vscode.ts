666df333efdd8d5d0bac59b06127ab927ac34b1a
Onso37 <afonso.vo@gmail.com>
2024-12-08 17:27:30 +0000

    sara extension

---
+++ x-men/sara/src/utils/vscode.ts
@@ -0,0 +1,18 @@
+import * as fs from "fs";
+import * as vscode from "vscode";
+import * as path from "path";
+
+export function getTemplate(fileName: string, webview: vscode.Webview, dirName: string): string {
+    const extensionUri = vscode.Uri.file(path.join(dirName, "../../"));
+    const templatePath = vscode.Uri.joinPath(extensionUri, 'public', 'template', fileName + '.html');
+    const styleUri = webview.asWebviewUri(vscode.Uri.joinPath(extensionUri, 'public', 'css', fileName +'.css'));
+    const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(extensionUri, 'public', 'js', fileName + '.js'));
+    const imageUri = webview.asWebviewUri(vscode.Uri.joinPath(extensionUri, 'public', 'images')) + "/";
+
+    const content = fs.readFileSync(templatePath.fsPath, 'utf-8');
+
+    return content
+        .replace('{{styleUri}}', styleUri.toString())
+        .replace('{{scriptUri}}', scriptUri.toString())
+        .replaceAll('{{imageUri}}', imageUri.toString());
+}
