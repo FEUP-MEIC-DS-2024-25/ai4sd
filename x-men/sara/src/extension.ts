bae7c193d92435bfecd8ace149cb6949bd278bc5
Afonso Osorio <up202108700@edu.fe.up.pt>
2024-12-12 16:56:37 +0000

    more robust activation

--- x-men/sara/src/extension.ts
+++ x-men/sara/src/extension.ts
@@ -5,18 +5,22 @@ import { execute } from './SARA/SARA';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "sara" is now active!');
 
-	const baseExt = vscode.extensions.getExtension("undefined_publisher.ai4sd-analysers")?.exports;
+	let baseExt = vscode.extensions.getExtension("undefined_publisher.ai4sd-analysers");
 	if (!baseExt) {
-		throw new Error("Base AI4SD extension not found.");
+		await vscode.commands.executeCommand("ai4sd-analysers.wakeup");
+		baseExt = vscode.extensions.getExtension("undefined_publisher.ai4sd-analysers");
+		if (!baseExt) {
+			throw new Error("Base AI4SD extension not found.");
+		}
 	}
 
-	baseExt.register({label: "SARA", description: "Executes SARA superhero", execute: () => execute(context)});
+	baseExt.exports.register({label: "SARA", description: "Executes SARA superhero", execute: () => execute(context)});
 }
 
 // This method is called when your extension is deactivated
