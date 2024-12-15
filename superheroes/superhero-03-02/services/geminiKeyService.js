76d89072b69cabcbacdf5dbf991effe9919c5e4e
Hugo HOVHANNESSIAN <hugo.hovhannessian@etu.univ-tours.fr>
2024-12-15 16:34:41 +0000

    Add : Add the file that can have access to the secret

---
+++ superheroes/superhero-03-02/services/geminiKeyService.js
@@ -0,0 +1,23 @@
+const name = 'projects/150699885662/secrets/superhero-03-02-secre/versions/latest';
+// const name = 'projects/my-project/secrets/my-secret/versions/latest';
+
+// Imports the Secret Manager library
+const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
+
+// Instantiates a client
+const client = new SecretManagerServiceClient();
+
+async function accessSecretVersion() {
+    const [version] = await client.accessSecretVersion({
+        name: name,
+    });
+
+    // Extract the payload as a string.
+    const payload = version.payload.data.toString();
+
+    // WARNING: Do not print the secret in a production environment - this
+    // snippet is showing how to access the secret material.
+    // console.info(`Payload: ${payload}`);
+}
+
+accessSecretVersion();
