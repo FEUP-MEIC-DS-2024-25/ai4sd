c0c0589f41a8761ed54ee31a16376e43c6b5821d
jaimefrf <jaimefrfonseca03@gmail.com>
2024-12-17 16:17:16 +0000

    reset backend files with correct port

---
+++ superheroes/superhero-06-03/api/serializers.py
@@ -0,0 +1,11 @@
+# api/serializers.py
+
+from rest_framework import serializers
+from .models import Item
+
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = '__all__'
+
+
