00caf8d9bbc54b501023926f4a5a499e5048e0a6
David Cordeiro <davcordeiro03@gmail.com>
2024-12-18 21:19:00 +0000

    Move topic name to consts

--- jarvis/consts.js
+++ jarvis/consts.js
@@ -4,6 +4,7 @@ import { PubSub } from '@google-cloud/pubsub'
 const GITHUB_APP_PRIVATE_KEY_PATH = "jarvis-fetcher/keys/github_app_private.pem"
 const SERVICE_ACCOUNT_KEY_PATH = "jarvis-key-02-02.json";
 const BUCKET_NAME = "hero-alliance-nexus";
+const ECHO_JARVIS_TOPIC = "echo-jarvis";
 const DOWNLOAD_DIR = "/downloads";
 const PASSPHRASE = "jarvis-secrets";
 
@@ -15,4 +16,4 @@ const PUBSUB_CLIENT = new PubSub({
     keyFilename: SERVICE_ACCOUNT_KEY_PATH,
 });
 
-export { GITHUB_APP_PRIVATE_KEY_PATH, SERVICE_ACCOUNT_KEY_PATH, BUCKET_NAME, DOWNLOAD_DIR, PASSPHRASE, STORAGE_CLIENT, PUBSUB_CLIENT };
+export { GITHUB_APP_PRIVATE_KEY_PATH, SERVICE_ACCOUNT_KEY_PATH, BUCKET_NAME, ECHO_JARVIS_TOPIC, DOWNLOAD_DIR, PASSPHRASE, STORAGE_CLIENT, PUBSUB_CLIENT };
--- jarvis/jarvis-publisher/publisher.js
+++ jarvis/jarvis-publisher/publisher.js
@@ -1,15 +1,14 @@
-import { PUBSUB_CLIENT } from '../consts';
+import { ECHO_JARVIS_TOPIC, PUBSUB_CLIENT } from '../consts';
 
 /**
  * Publishes a message to the 'echo-jarvis' Pub/Sub topic.
  * @param {object} message - The message object to publish.
  */
 export async function publishToEchoJarvis(message) {
-    const topicName = 'echo-jarvis';
     const dataBuffer = Buffer.from(JSON.stringify(message));
 
     try {
-        const messageId = await PUBSUB_CLIENT.topic(topicName).publishMessage(dataBuffer);
+        const messageId = await PUBSUB_CLIENT.topic(ECHO_JARVIS_TOPIC).publishMessage(dataBuffer);
         console.log(`Message ${messageId} published to topic ${topicName}.`)
     } catch (error) {
         console.error(`Error publishing message to ${topicName}:`, error.message)
