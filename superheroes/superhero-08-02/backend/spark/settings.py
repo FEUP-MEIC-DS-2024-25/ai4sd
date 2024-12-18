09de65759a541154d4f9c9ffa5b8a4a1decad605
José Francisco Veiga <veigajosefrancisco@gmail.com>
2024-12-18 11:57:59 +0000

    Debug False

--- superheroes/superhero-08-02/backend/spark/settings.py
+++ superheroes/superhero-08-02/backend/spark/settings.py
@@ -27,7 +27,7 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 if DEBUG: # Running in development
     SECURE_SSL_REDIRECT = False
