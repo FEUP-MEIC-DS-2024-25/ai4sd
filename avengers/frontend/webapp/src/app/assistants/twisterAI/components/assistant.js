3ffbec88d42e8c3ebe315486abb6633151d8d30e
ema-12-martins <a97678@alunos.uminho.pt>
2024-12-18 15:25:40 +0000

    base de front-end

--- avengers/frontend/webapp/src/app/assistants/twisterAI/components/assistant.js
+++ avengers/frontend/webapp/src/app/assistants/twisterAI/components/assistant.js
@@ -1,9 +1,60 @@
 import styles from "@/app/page.module.css";
+import "./assistant.css"
+
 export default function Assistant() {
-    return (
-        <div className={styles.assistantInteraction}>
-            <h1 className="text-3xl font-bold text-indigo-600">YOUR ASSISTANT</h1>
-            <p>Example component for YOUR ASSISTANT</p>
+    
+  return (
+    <div className="TwisterAI">
+      <header>
+        <div>
+          <h1>TwisterAI</h1>
         </div>
-    )
+        <div>
+          <nav>
+            <label>Select the language:</label>
+            <select id="language">
+              <option value="java">Java</option>
+              <option value="javascript">JavaScript</option>
+              <option value="python">Python</option>
+            </select>
+          </nav>
+        </div>
+      </header>
+      <main>
+        <section>
+          <div>
+            <label className="upload" htmlFor="code-file">
+              Upload code file<i className="fa-solid fa-paperclip"></i>
+            </label>
+            <input
+                className="upload_btn"
+                type="file"
+                // onChange={(e) => handleFileChange(e, 'code')}
+                />
+          </div>
+          <div>
+            <label className="upload" htmlFor="tests-file">
+              Upload tests file<i className="fa-solid fa-paperclip"></i>
+            </label>
+            <input
+                className="upload_btn"
+                type="file"
+                 // onChange={(e) => handleFileChange(e, 'test')}
+                />
+          </div>
+        </section>
+
+        <section>
+            <label htmlFor="context">Give me some context about your code:</label>
+            <textarea id="context" placeholder="Insert here the context."></textarea>
+        </section>
+
+        <section>
+            <button>Generate mutant tests</button> 
+            <button>Download mutant tests</button>
+        </section>
+
+      </main>
+    </div>
+  );
 }
