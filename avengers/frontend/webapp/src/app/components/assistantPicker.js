100211f1840dcd6a382c4e3828c6b3b305777c47
André Leonor <76494100+andre13l@users.noreply.github.com>
2024-12-18 20:01:02 +0000

    Update assistantPicker.js

--- avengers/frontend/webapp/src/app/components/assistantPicker.js
+++ avengers/frontend/webapp/src/app/components/assistantPicker.js
@@ -30,7 +30,7 @@ function getAssistantList() {
     let ai15 = { name: "SPARK", type: "req", link: "/assistants/spark" };
     let ai16 = { name: "BANANAI", type: "req", link: "/assistants/bananai" };
 
-    return [ai1, ai2, ai3, ai4, ai5, ai6, ai7, ai8, ai9, ai10, ai11, ai12, ai13, ai14, a15, a16]
+    return [ai1, ai2, ai3, ai4, ai5, ai6, ai7, ai8, ai9, ai10, ai11, ai12, ai13, ai14, ai15, ai16]
 }
 
 function AssistantPickerItems() {
