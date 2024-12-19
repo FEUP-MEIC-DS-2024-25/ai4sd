8f70e28035d27125a5441701f5e78aeb4ff923ac
filipecardoso0 <>
2024-12-19 14:12:08 +0000

    Backend integration with Firebase

--- superheroes/superhero-03-03/src/app.js
+++ superheroes/superhero-03-03/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 var cors = require('cors')
 const promptRoutes = require('./routes/geminiRoutes');
+const chatRoutes = require('./routes/chatRoutes');
 
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../..', '.env') });
@@ -12,9 +13,9 @@ const PORT = 8080;
 app.use(express.json());
 app.use(cors())
 
-
 // Routes
 app.use('/api', promptRoutes);
+app.use('/req2speech/chat', chatRoutes);
 
 // Start the server
 app.listen(PORT, () => {
