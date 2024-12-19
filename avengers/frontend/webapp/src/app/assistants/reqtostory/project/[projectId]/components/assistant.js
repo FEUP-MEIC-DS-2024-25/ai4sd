3a7f959303285913f187a3bd25e5181481dd7a1b
Tiago Azevedo <up202108699@edu.fe.up.pt>
2024-12-19 18:58:52 +0000

    add code (not worke) to save user stories updates

--- avengers/frontend/webapp/src/app/assistants/reqtostory/project/[projectId]/components/assistant.js
+++ avengers/frontend/webapp/src/app/assistants/reqtostory/project/[projectId]/components/assistant.js
@@ -15,8 +15,8 @@ const Assistant = () => {
     const [requirements, setRequirements] = useState({});
     const [userStories, setUserStories] = useState([]);
 
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const [userStoryIndex, setUserStoryIndex] = useState(0);
+    const [reqVersion, setReqVersion] = useState(0);
+    const [userStoriesVersion, setUserStoriesVersion] = useState(0);
     const numberVersion = useRef(0);
 
     const { projectId } = useParams();
@@ -26,7 +26,7 @@ const Assistant = () => {
     const version = null;
 
     const [isEditing, setIsEditing] = useState(false);
-    const [editContent, setEditContent] = useState(requirements.content);
+    const [editReq, setEditReq] = useState(requirements.content);
 
     const [editingStory, setEditingStory] = useState(null);
     const [tempContent, setTempContent] = useState("");
@@ -40,31 +40,32 @@ const Assistant = () => {
         } else {
             setVersions(version);
         }
-        setCurrentIndex(0);
-        setUserStoryIndex(0);
+        setReqVersion(0);
+        setUserStoriesVersion(0);
     }, [projectId, version]);
 
     useEffect(() => {
         if (versions.length === 0) return;
         setRequirements({
-            content: versions[currentIndex].content,
-            version: versions[currentIndex].version,
+            content: versions[reqVersion].content,
+            version: versions[reqVersion].version,
         });
-        numberVersion.current = versions[currentIndex].user_stories.length;
-        setEditContent(versions[currentIndex].content);
-    }, [currentIndex, versions]);
+        if (!versions[reqVersion].user_stories) return;
+        numberVersion.current = versions[reqVersion].user_stories.length;
+        setEditReq(versions[reqVersion].content);
+    }, [reqVersion, versions]);
 
     useEffect(() => {
         if (
             versions.length === 0 ||
-            !versions[currentIndex] ||
-            !versions[currentIndex].user_stories
+            !versions[reqVersion] ||
+            !versions[reqVersion].user_stories
         )
             return;
         const userStories =
-            versions[currentIndex].user_stories[userStoryIndex].user_stories;
-        setUserStories(JSON.parse(userStories));
-    }, [currentIndex, versions, userStoryIndex]);
+            versions[reqVersion].user_stories[userStoriesVersion].user_stories;
+        setUserStories(userStories);
+    }, [reqVersion, versions, userStoriesVersion]);
 
     const fetchProjectContent = async (projectId) => {
         try {
@@ -85,35 +86,35 @@ const Assistant = () => {
     };
 
     const goBack = () => {
-        if (currentIndex > 0) {
-            setCurrentIndex(currentIndex - 1);
-            setUserStoryIndex(0);
+        if (reqVersion > 0) {
+            setReqVersion(reqVersion - 1);
+            setUserStoriesVersion(0);
         }
     };
 
     const goForward = () => {
-        if (currentIndex < versions.length - 1) {
-            setCurrentIndex(currentIndex + 1);
-            setUserStoryIndex(0);
+        if (reqVersion < versions.length - 1) {
+            setReqVersion(reqVersion + 1);
+            setUserStoriesVersion(0);
         }
     };
 
     const goBackUS = () => {
-        if (userStoryIndex > 0) {
-            setUserStoryIndex(userStoryIndex - 1);
+        if (userStoriesVersion > 0) {
+            setUserStoriesVersion(userStoriesVersion - 1);
         }
     };
 
     const goForwardUS = () => {
-        if (userStoryIndex < numberVersion.current - 1) {
-            setUserStoryIndex(userStoryIndex + 1);
+        if (userStoriesVersion < numberVersion.current - 1) {
+            setUserStoriesVersion(userStoriesVersion + 1);
         }
     };
 
     const handleSubmit = async (projId, reqVersion, newContent) => {
         try {
             setIsLoading(true);
-            let content = editContent;
+            let content = editReq;
 
             if (!newContent) {
                 content = requirements.content;
@@ -160,6 +161,7 @@ const Assistant = () => {
                 ...updatedVersions.map((v) => v.version),
                 0
             );
+            console.log(user_stories);
             const newReqVersion = {
                 version: lastVersion + 1,
                 content: req,
@@ -181,6 +183,8 @@ const Assistant = () => {
                 });
             }
         }
+        console.log(versions)
+        console.log(updatedVersions)
 
         setVersions(updatedVersions);
     };
@@ -206,7 +210,7 @@ const Assistant = () => {
 
     const handleCancelClick = () => {
         setIsEditing(false);
-        setEditContent(requirements.content);
+        setEditReq(requirements.content);
     };
 
     return (
@@ -235,8 +239,8 @@ const Assistant = () => {
                             <p>Project's Requirements</p>
                             <textarea
                                 className="bg-[#2f2f2f] text-[#e1e1e1] border-4 border-[#2f2f2f] rounded-[20px] mx-auto p-2 w-[80%] resize-none"
-                                value={editContent}
-                                onChange={(e) => setEditContent(e.target.value)}
+                                value={editReq}
+                                onChange={(e) => setEditReq(e.target.value)}
                             />
                             <div className="flex justify-end items-center mx-auto my-2 w-[80%]">
                                 <button
@@ -256,7 +260,6 @@ const Assistant = () => {
                     ) : (
                         <div className="w-[100%]">
                             <p>Project's Requirements</p>
-                            {console.log(requirements.content)}
                             <textarea
                                 className="bg-[#e1e1e1] text-[#2f2f2f] border-4 border-[#e1e1e1] rounded-[20px] mx-auto p-2 w-[80%] resize-none"
                                 placeholder={requirements.content}
@@ -272,17 +275,17 @@ const Assistant = () => {
                                 </button>
                                 <button
                                     onClick={goBack}
-                                    disabled={currentIndex === 0}
+                                    disabled={reqVersion === 0}
                                 >
                                     ⮜{" "}
                                 </button>
                                 <span>
-                                    {currentIndex + 1}/{versions.length}
+                                    {reqVersion + 1}/{versions.length}
                                 </span>
                                 <button
                                     onClick={goForward}
                                     disabled={
-                                        currentIndex === versions.length - 1
+                                        reqVersion === versions.length - 1
                                     }
                                 >
                                     {" "}
@@ -305,22 +308,25 @@ const Assistant = () => {
                             setEditingStory={setEditingStory}
                             tempContent={tempContent}
                             setTempContent={setTempContent}
+                            projectId = {projectId}
+                            reqVersion = {requirements.version}
+                            userStoriesVersion = {userStoriesVersion}
                         />
                     </div>
                     <div className="versionSelect">
                         <button
                             onClick={goBackUS}
-                            disabled={userStoryIndex === 0}
+                            disabled={userStoriesVersion === 0}
                         >
                             ⮜{" "}
                         </button>
                         <span>
-                            {userStoryIndex + 1}/{numberVersion.current}
+                            {userStoriesVersion + 1}/{numberVersion.current}
                         </span>
                         <button
                             onClick={goForwardUS}
                             disabled={
-                                userStoryIndex === numberVersion.current - 1
+                                userStoriesVersion === numberVersion.current - 1
                             }
                         >
                             {" "}
