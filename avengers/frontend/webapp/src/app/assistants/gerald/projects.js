78a1c1e9da9786193cfb790288bd77a0d8fc7dc0
FranciscoCardoso913 <franciscocardoso.3003@gmail.com>
2024-12-19 00:13:59 +0000

    Gerald project page base design

---
+++ avengers/frontend/webapp/src/app/assistants/gerald/projects.js
@@ -0,0 +1,16 @@
+import styles from "@/app/page.module.css";
+import 'bootstrap/dist/css/bootstrap.css';
+
+import '@/app/globals.css';
+
+
+import ProjectPicker from "./components/projectPicker";
+
+export default function Projects() {
+    return(
+        <div>
+          
+            <ProjectPicker></ProjectPicker>
+        </div>
+    )
+}
