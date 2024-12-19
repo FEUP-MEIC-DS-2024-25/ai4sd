8d5fdd06ff4d3701229b0f8fc0d3f2a051b3b40c
Ricardo Peralta <up202206392@up.pt>
2024-12-19 13:55:35 +0000

    Deployment URL

--- avengers/frontend/webapp/src/app/assistants/featurecraft/hooks/useAssistChatDelete.js
+++ avengers/frontend/webapp/src/app/assistants/featurecraft/hooks/useAssistChatDelete.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default async function deleteChat(conversationId, setError) {
     try {
-        const response = await axios.delete(`http://localhost:8080/chat/${conversationId}`);
+        const response = await axios.delete(`https://superhero-03-01-150699885662.europe-west1.run.app/${conversationId}`);
         return response.data;
     } catch (error) {
         setError(error.response ? error.response.data : "An error occurred");
