fa9ddac61a9dc238b40cf05741a19632168c24ff
jaimefrf <jaimefrfonseca03@gmail.com>
2024-12-19 15:59:38 +0000

    rollback to functional code

--- superheroes/superhero-06-03/api/urls.py
+++ superheroes/superhero-06-03/api/urls.py
@@ -1,10 +1,9 @@
 # api/urls.py
 
 from django.urls import path
-from .views import ReviewView, ReportsView
+from .views import ReviewView
 
 urlpatterns = [
     path('get-review/', ReviewView.as_view(), name='get-review'),
-    path('get-client-reports/', ReportsView.as_view(), name='get-client-reports')
 ]
 
