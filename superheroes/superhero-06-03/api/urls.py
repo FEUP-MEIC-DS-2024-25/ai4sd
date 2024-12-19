8aecdbcb11e17a53587d4cb2f817545668b00eda
jaimefrf <jaimefrfonseca03@gmail.com>
2024-12-19 15:15:28 +0000

    attempt to change url

--- superheroes/superhero-06-03/api/urls.py
+++ superheroes/superhero-06-03/api/urls.py
@@ -5,6 +5,6 @@ from .views import ReviewView, ReportsView
 
 urlpatterns = [
     path('get-review/', ReviewView.as_view(), name='get-review'),
-    path('get-client-reports/', ReportsView.as_view(), name='get-reports')
+    path('get-client-reports/', ReportsView.as_view(), name='get-client-reports')
 ]
 
