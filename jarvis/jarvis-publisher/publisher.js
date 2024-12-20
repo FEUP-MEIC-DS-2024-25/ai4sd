2c1a74113a0f3b8a9fc3921c4ffc1c94f5a965fc
Mª Rita Lopes <up202108819@up.pt>
2024-12-20 01:22:19 +0000

    Bug fixes

--- jarvis/jarvis-publisher/publisher.js
+++ jarvis/jarvis-publisher/publisher.js
@@ -7,11 +7,16 @@ import { ECHO_JARVIS_TOPIC, PUBSUB_CLIENT } from '../consts.js';
 export async function publishToEchoJarvis(message) {
     const dataBuffer = Buffer.from(JSON.stringify(message));
 
+    // Wrap the dataBuffer in an object with a 'data' field for Pub/Sub
+    const pubsubMessage = {
+        data: dataBuffer,
+    };
+
     try {
-        const messageId = await PUBSUB_CLIENT.topic(ECHO_JARVIS_TOPIC).publishMessage(dataBuffer);
-        console.log(`Message ${messageId} published to topic ${topicName}.`)
+        const messageId = await PUBSUB_CLIENT.topic(ECHO_JARVIS_TOPIC).publishMessage(pubsubMessage);
+        console.log(`Message ${messageId} published to topic ${ECHO_JARVIS_TOPIC}.`)
     } catch (error) {
-        console.error(`Error publishing message to ${topicName}:`, error.message)
+        console.error(`Error publishing message to ${ECHO_JARVIS_TOPIC}:`, error.message)
         throw error;
     }
 }
