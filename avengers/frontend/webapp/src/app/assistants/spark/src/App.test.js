1ad30f93a91b41a3be2c32047c9ea1f908dff38b
joaoalvesss <up202108670@up.pt>
2024-12-20 03:40:16 +0000

    trying to solve problem in frontend 2

--- avengers/frontend/webapp/src/app/assistants/spark/src/App.test.js
+++ avengers/frontend/webapp/src/app/assistants/spark/src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../components/Spark';
 
 test('renders learn react link', () => {
   render(<App />);
