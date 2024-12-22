6731a47c834a75fa4c9f65eac1dbe7df699ed361
Mª Rita Lopes <up202108819@up.pt>
2024-12-22 19:24:24 +0000

    Deleted old ai4sd extension folder

--- x-men/ai4sd/public/js/menu.js
+++
@@ -1,7 +0,0 @@
-const vscode = acquireVsCodeApi();
-
-document.getElementById('submit').addEventListener('click', () => {
-    const loc = document.getElementById('location');
-    const locOption = loc.options[loc.selectedIndex].innerHTML;
-    vscode.postMessage({ command: 'location', text: locOption });
-});
